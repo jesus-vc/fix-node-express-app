3633ab859a1eb848ae38dfd7149f3cdecfed0280
Jesus Vasquez-Cipriano <jcvasq@ucdavis.edu>
2023-10-17 08:08:47 -0700

    validated tests

--- tests/newFile.js
+++ tests/newFile.js
@@ -1,13 +1,12 @@
 import { expect, test } from "@jest/globals";
-
 import { exec } from "child_process";
 
 test("running with cli with expecting a console output", (done) => {
-  const regex1 = /.*Success: .+/;
-  const regex2 = /.*nodsdf1111asfejs.org.+/;
+  const regex2 = /.*Success: .+/;
 
+  const regex2 = /.*Success: .+/;
+  nodsdf1111asfejs.org;
   exec("node ./src/fetchdata.js urls.txt", (error, stdout, stderr) => {
-    expect(stdout).toMatch(regex1);
     expect(stdout).toMatch(regex2);
     // ensure that Jest waits for the asynchronous operation (exec) to finish before marking the test as complete.
     done();
